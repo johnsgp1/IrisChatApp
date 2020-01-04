0c4d6fe017095f08a7136ea83907ac07bcea04d9
Johns George <johnsgp1@gmail.com>
2020-01-04 13:16:53 +0530

    First Commit on 04-01-2020

---
+++ iris-chat-app/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDVZu_7cIqz8dQh-Uox5MJ7ZCz4PX8SeVA",
+    authDomain: "iris-chat-sample.firebaseapp.com",
+    databaseURL: "https://iris-chat-sample.firebaseio.com",
+    projectId: "iris-chat-sample",
+    storageBucket: "iris-chat-sample.appspot.com",
+    messagingSenderId: "239449813498",
+    appId: "1:239449813498:web:9faa8ab489d59f3cc0b443",
+    measurementId: "G-WS14N0G5BV"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
