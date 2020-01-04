848032a5ab988d02710013786a9cb59554c70e28
Johns George <johnsgp1@gmail.com>
2020-01-04 16:45:52 +0530

    Second Commit

--- iris-chat-app/src/app/app.module.ts
+++ iris-chat-app/src/app/app.module.ts
@@ -3,16 +3,30 @@ import { NgModule } from '@angular/core';
 import { AngularFireModule } from 'angularfire2';
 import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
+//import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { ChatComponent } from './chat/chat.component';
+import { RouterModule } from '@angular/router'
+import { routes } from './routes';
+import {FormsModule} from '@angular/forms';
+import {AuthService} from '../app/auth.service';
+import {ChatService } from '../app/chat.service'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
-    AngularFireModule.initializeApp(environment.firebase)
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    RouterModule.forRoot(routes),
+    AngularFireDatabaseModule
   ],
-  providers: [],
+  providers: [AuthService,ChatService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ iris-chat-app/src/app/routes.ts
@@ -0,0 +1,5 @@
+import{Routes} from '@angular/router';
+import { ChatComponent } from './chat/chat.component';
+export const routes: Routes = [
+    { path: 'chats/:id', component: ChatComponent }
+  ];
